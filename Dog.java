a2a9ae3c54e7debcaea4a35eb2f5b26a577e3eb8
barasapeter <barasapeter52@gmail.com>
2025-10-24 14:39:45 +0300

    Arrays are objects - end of story

--- Dog.java
+++ Dog.java
@@ -1,29 +1,19 @@
 public class Dog {
     String name = "Bosco";
-    String owner = "Peter";
-    int age = 2;
 
-    void bark() {
-        System.out.println("Ruff! Ruff!");
-    }
-
-    void itIsBirthday() {
-        this.bark();
-        this.age += 1;
+    void bark(int numOfBarks) {
+        int i = 0;
+        while (i < numOfBarks) {
+            i++;
+            System.out.println("ruff");
+        }
     }
 
     public static void main(String[] args) {
-        Dog mommyDog = new Dog();
 
-        Dog dogCopy = mommyDog;
+        Dog slurp = new Dog();
 
-        System.out.println(mommyDog.age);
-
-        mommyDog.itIsBirthday();
-
-        System.out.println(mommyDog.age);
-
-        System.out.println(dogCopy.age);
+        slurp.bark(5);
 
     }
 }
